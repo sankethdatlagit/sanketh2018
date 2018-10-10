03636a7ef373de98ec27fc776390861f879e5706
Sanketh <sankethdatla@gmail.com>
2018-10-10 16:10:01 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'About',component:AboutComponent},
+      {path:'Home',component:HomeComponent},
+      {path:'',redirectTo:'Home',pathMatch:'full'},
+      {path:"**",redirectTo:'Home',pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
